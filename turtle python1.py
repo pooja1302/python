df922fa1cefbdf645299f94913121a7073f29111
pooja1302 <51992054+pooja1302@users.noreply.github.com>
2019-06-29 14:19:44 +0530

    Add files via upload

---
+++ turtle python1.py
@@ -0,0 +1,15 @@
+import turtle as tt
+a1=tt.Turtle()
+a1.backward(150)
+a1.left(90)
+
+a1.backward(150)
+a1.left(90)
+
+a1.backward(150)
+a1.left(90)
+a1.backward(150)
+a1.left(90)
+a1.backward(150)
+a1.left(90)
+tt.done()
